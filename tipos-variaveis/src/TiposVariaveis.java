66d3ff424bbb24992c40a2a369f276d3296870f3
Lazaro <lzjunior.007@gmail.com>
2024-08-08 01:42:28 -0300

    final da aula sobre tipos de variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,3 +1,26 @@
 public class TiposVariaveis {
-    
+
+    public static void main(String[] args) throws Exception {
+        //tipos primitivos
+        //estudem a classe string que representa texto na aplicação
+
+    String meuNome = "Lazaro";
+    System.out.print(meuNome);
+
+    double salarioMinimo = 2500;
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short) numeroNormal;
+
+    int numero = 5;
+
+    numero = 10;
+
+    System.out.print(numero);
+
+    final double  VALOR_DE_PI = 3.14;
+
+
+    }
 }
